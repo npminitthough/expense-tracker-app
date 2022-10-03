245bd1ed33c8b57009638401053269076c3ca45a
npminitthough <tasnim.sultanah@gmail.com>
2022-10-03 15:26:12 +0100

    Initial app with navigator packages installed.

---
+++ screens/AllExpenses.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function AllExpenses(props) {
+  return <Text>All expenses screen</Text>;
+}
---
+++ screens/ManageExpense.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function ManageExpense(props) {
+  return <Text>Manage expenses</Text>;
+}
